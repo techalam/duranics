2096d18af890fd52d6213869e778b28f42a62a1b
Faizan Alam <106431643+techalam@users.noreply.github.com>
2023-01-31 23:42:10 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,34 @@
-/*vanilla-tilt.js*/
-const tilt = document.querySelectorAll(".tilt");
+// /*vanilla-tilt.js*/
+// const tilt = document.querySelectorAll(".tilt");
 
-VanillaTilt.init(tilt, {
-	reverse: true,
-	max: 15,
-	speed: 400,
-	scale: 1.12,
-	glare: true,
-	reset: true,
-	perspective: 500,
-	transition: true,
-	"max-glare": 0.75,
-	"glare-prerender": false,
-	gyroscope: true,
-	gyroscopeMinAngleX: -45,
-	gyroscopeMaxAngleX: 45,
-	gyroscopeMinAngleY: -45,
-	gyroscopeMaxAngleY: 45
-});
+// VanillaTilt.init(tilt, {
+// 	reverse: true,
+// 	max: 15,
+// 	speed: 400,
+// 	scale: 1.12,
+// 	glare: true,
+// 	reset: true,
+// 	perspective: 500,
+// 	transition: true,
+// 	"max-glare": 0.75,
+// 	"glare-prerender": false,
+// 	gyroscope: true,
+// 	gyroscopeMinAngleX: -45,
+// 	gyroscopeMaxAngleX: 45,
+// 	gyroscopeMinAngleY: -45,
+// 	gyroscopeMaxAngleY: 45
+// });
 
 
-function sorting(){
-	var sorting = document.getElementById('sort');
-if(sorting.style.display == "block"){
-	sorting.style.display = "none";
-}
-else{
-	sorting.style.display = "block";
-}
-	}
+// function sorting(){
+// 	var sorting = document.getElementById('sort');
+// if(sorting.style.display == "block"){
+// 	sorting.style.display = "none";
+// }
+// else{
+// 	sorting.style.display = "block";
+// }
+// 	}
 
 
 
