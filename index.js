c8ba68143beff410f204cc62857add9aa113cb6a
Faizan Alam <106431643+techalam@users.noreply.github.com>
2023-02-12 12:50:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var mysql = require('mysql2');
 const multer = require("multer");
+const session = require('express-session');
 // const port = process.env.PORT || 3000;
 var SESSION_SECRET_KEY = "asdfgthftdgcvfgrtdgshycgsfdrefdh";
 // import {createPool} from 'mysql2/promise';
@@ -43,6 +44,16 @@ const con = mysql.createConnection({
 // })
 
 var app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -115,8 +126,60 @@ con.connect();
 app.get('/enquiry', (req, res) => {
   res.render('pages/enquiry');
 });
-app.get('/admin',(req,res) =>{
-res.render('pages/admin');
+app.get('/admin', (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect('/login');
+    return;
+  }
+
+  // render the admin page here
+  res.send('Admin Page');
+});
+
+app.get('/login', (req, res) => {
+  res.send(`
+  <script src="https://cdn.tailwindcss.com"></script>
+
+    <section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form method="POST" action="/login" class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                      <input name="username" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your username" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+
+                  <div class="mb-4" style="width: 50%; margin: auto;">
+                  <button class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-700 leading-tight focus:outline-none focus:shadow-outline" id="submit" name="submit" type="submit">submit</button>
+                </div>
+
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
+  `);
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // check username and password here (e.g., by querying a database)
+  if (username === 'admin' && password === 'password') {
+    req.session.authenticated = true;
+    res.render('pages/admin');
+  } else {
+    res.send('Invalid username or password');
+  }
 });
 
 app.post('/submit', (req, res) => {
