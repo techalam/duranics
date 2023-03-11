ddb224eb3a70225fc4b398b62168fac62ab6abc7
Faizan Alam <106431643+techalam@users.noreply.github.com>
2023-03-11 15:24:22 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,23 @@
-// /*vanilla-tilt.js*/
-// const tilt = document.querySelectorAll(".tilt");
+/*vanilla-tilt.js*/
+const tilt = document.querySelectorAll(".tilt");
 
-// VanillaTilt.init(tilt, {
-// 	reverse: true,
-// 	max: 15,
-// 	speed: 400,
-// 	scale: 1.12,
-// 	glare: true,
-// 	reset: true,
-// 	perspective: 500,
-// 	transition: true,
-// 	"max-glare": 0.75,
-// 	"glare-prerender": false,
-// 	gyroscope: true,
-// 	gyroscopeMinAngleX: -45,
-// 	gyroscopeMaxAngleX: 45,
-// 	gyroscopeMinAngleY: -45,
-// 	gyroscopeMaxAngleY: 45
-// });
+VanillaTilt.init(tilt, {
+	reverse: true,
+	max: 15,
+	speed: 400,
+	scale: 1.12,
+	glare: true,
+	reset: true,
+	perspective: 500,
+	transition: true,
+	"max-glare": 0.75,
+	"glare-prerender": false,
+	gyroscope: true,
+	gyroscopeMinAngleX: -45,
+	gyroscopeMaxAngleX: 45,
+	gyroscopeMinAngleY: -45,
+	gyroscopeMaxAngleY: 45
+});
 
 
 function sorting(){
@@ -36,6 +36,38 @@ function showFilters() {
     var listfilter = document.getElementById("filterContain");
     listbg.classList.toggle("mystyle");
     listfilter.classList.toggle("mystyle2");
+}
+
+
+    var carousel = setInterval(() => {
+        document.getElementById('nextSlide').click();
+    }, 3000);
+
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function prevSlide() {
+  showSlide(slideIndex -= 1);
+}
+
+function nextSlide() {
+  showSlide(slideIndex += 1);
+  clearInterval(carousel);
+}
+
+function showSlide(n) {
+  let slides = document.getElementsByClassName("slideshow-slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideIndex - 1].classList.add("active");
 }
 
 
